76948a53e763915a2b17d243db1434fe0efdcd32
niteshrana657-star <niteshrana657@gmail.com>
2026-07-27 19:03:52 +0530

    Add AccessibilityService class with enable/disable methods

--- services/AccessibilityService.ts
+++ services/AccessibilityService.ts
@@ -1,1 +1,19 @@
+export class AccessibilityService {
+  private enabled = false;
 
+  enable(): void {
+    this.enabled = true;
+  }
+
+  disable(): void {
+    this.enabled = false;
+  }
+
+  isEnabled(): boolean {
+    return this.enabled;
+  }
+
+  getStatus(): string {
+    return this.enabled ? "Enabled" : "Disabled";
+  }
+}
