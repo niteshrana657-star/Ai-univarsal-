9fe2802c7e06f9de1cde736ff89b4af4b85c2438
niteshrana657-star <niteshrana657@gmail.com>
2026-07-27 14:48:42 +0530

    Add Sidebar component with navigation links

---
+++ components/Sidebar.tsx
@@ -0,0 +1,14 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>AI Assistant</li>
+          <li>Permissions</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
