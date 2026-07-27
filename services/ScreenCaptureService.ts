8fccf12dfd4af87ea537163cb6a0ac3f2a1ee243
niteshrana657-star <niteshrana657@gmail.com>
2026-07-27 19:05:47 +0530

    Implement ScreenCaptureService with capture control methods

--- services/ScreenCaptureService.ts
+++ services/ScreenCaptureService.ts
@@ -1,1 +1,19 @@
+export class ScreenCaptureService {
+  private capturing = false;
 
+  startCapture(): void {
+    this.capturing = true;
+  }
+
+  stopCapture(): void {
+    this.capturing = false;
+  }
+
+  isCapturing(): boolean {
+    return this.capturing;
+  }
+
+  getStatus(): string {
+    return this.capturing ? "Capturing" : "Stopped";
+  }
+}
