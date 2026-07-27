e11383d201587b5cfdff62fae8efb926dc488da9
niteshrana657-star <niteshrana657@gmail.com>
2026-07-27 18:56:49 +0530

    Implement PermissionService for managing permissions

--- permissions/PermissionService.ts
+++ permissions/PermissionService.ts
@@ -1,1 +1,20 @@
+import { PermissionManager } from "./PermissionManager";
+import { PermissionStorage } from "./PermissionStorage";
 
+export class PermissionService {
+  private manager = new PermissionManager();
+
+  grantPermission(permissionId: string): void {
+    this.manager.updatePermission(permissionId, true);
+    PermissionStorage.save(permissionId, true);
+  }
+
+  denyPermission(permissionId: string): void {
+    this.manager.updatePermission(permissionId, false);
+    PermissionStorage.save(permissionId, false);
+  }
+
+  isPermissionGranted(permissionId: string): boolean {
+    return PermissionStorage.get(permissionId);
+  }
+}
