3112f7a8786339f8dfb9afac7ba05781c57199b1
niteshrana657-star <niteshrana657@gmail.com>
2026-07-28 23:16:26 +0530

    Update index.ts

--- accessibility/index.ts
+++ accessibility/index.ts
@@ -1,1 +1,31 @@
+/**
+ * -------------------------------------------------------------
+ * Universal AI Operating Companion
+ * Accessibility Module
+ * File: index.ts
+ * -------------------------------------------------------------
+ */
 
+
+// Types
+export * from "./AccessibilityTypes";
+
+
+// Constants
+export * from "./AccessibilityConstants";
+
+
+// Events
+export * from "./AccessibilityEvents";
+
+
+// Permission
+export * from "./AccessibilityPermission";
+
+
+// Service
+export * from "./AccessibilityService";
+
+
+// Manager
+export * from "./AccessibilityManager";
