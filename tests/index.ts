9186d2cd4b44b6db8fc12560e5ef52e8a8f25861
niteshrana657-star <niteshrana657@gmail.com>
2026-07-29 16:23:13 +0530

    Create index.ts

---
+++ tests/index.ts
@@ -0,0 +1,61 @@
+/**
+ * -------------------------------------------------------------
+ * Universal AI Operating Companion
+ * Tests Module Entry
+ * File: index.ts
+ * -------------------------------------------------------------
+ *
+ * Public exports for Tests module.
+ * -------------------------------------------------------------
+ */
+
+
+// Test Types
+
+export type {
+    TestStatus,
+    TestCategory,
+    TestCase,
+    TestExecution,
+    TestSummary
+} from "./TestTypes";
+
+
+
+// Test Result
+
+export {
+    TestResult
+} from "./TestResult";
+
+
+
+// Test Suite
+
+export {
+    TestSuite
+} from "./TestSuite";
+
+
+
+// Test Runner
+
+export {
+    TestRunner
+} from "./TestRunner";
+
+
+
+// Test Reporter
+
+export {
+    TestReporter
+} from "./TestReporter";
+
+
+
+// Unit Tests
+
+export {
+    UnitTests
+} from "./UnitTests";
