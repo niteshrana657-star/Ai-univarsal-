aa1b6b203fcbd76b37edf75efd6479794c2f4a74
niteshrana657-star <niteshrana657@gmail.com>
2026-08-02 17:45:19 +0530

    Create index.ts for runtime module exports
    Add index.ts for exporting runtime modules

---
+++ ai-engine/runtime/index.ts
@@ -0,0 +1,28 @@
+/**
+ * -------------------------------------------------------------
+ * Universal AI Operating Companion
+ * AI Engine Module
+ * Runtime Exports
+ * -------------------------------------------------------------
+ */
+
+export * from "./RuntimeEngine";
+export * from "./RuntimeManager";
+export * from "./RuntimeState";
+export * from "./RuntimeContext";
+
+export {
+  default as RuntimeEngine
+} from "./RuntimeEngine";
+
+export {
+  default as RuntimeManager
+} from "./RuntimeManager";
+
+export {
+  default as RuntimeState
+} from "./RuntimeState";
+
+export {
+  default as RuntimeContext
+} from "./RuntimeContext";
