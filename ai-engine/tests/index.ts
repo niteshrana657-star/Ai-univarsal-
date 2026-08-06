c6dd4af63e82ce4ea93b1a2e294231795a2223f8
niteshrana657-star <niteshrana657@gmail.com>
2026-08-06 19:28:56 +0530

    Add index.ts to export AI Engine tests

---
+++ ai-engine/tests/index.ts
@@ -0,0 +1,13 @@
+/**
+ * -------------------------------------------------------------
+ * Universal AI Operating Companion
+ * AI Engine Module
+ * Tests Exports
+ * -------------------------------------------------------------
+ */
+
+export * from "./AIEngine.test";
+export * from "./Provider.test";
+export * from "./Runtime.test";
+export * from "./Model.test";
+export * from "./Utils.test";
