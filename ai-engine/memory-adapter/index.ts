9ccddb6d3e8625bb4223e7505381aadbcbc8a7a5
niteshrana657-star <niteshrana657@gmail.com>
2026-08-06 19:15:44 +0530

    Create index.ts for memory adapter exports
    Add index.ts to export memory adapter modules and types.

---
+++ ai-engine/memory-adapter/index.ts
@@ -0,0 +1,39 @@
+/**
+ * -------------------------------------------------------------
+ * Universal AI Operating Companion
+ * AI Engine Module
+ * Memory Adapter Exports
+ * -------------------------------------------------------------
+ */
+
+export {
+  default as MemoryAdapter
+} from "./MemoryAdapter";
+
+export type {
+  MemoryRecord
+} from "./MemoryAdapter";
+
+export {
+  default as MemoryContextAdapter
+} from "./MemoryContextAdapter";
+
+export type {
+  MemoryContext
+} from "./MemoryContextAdapter";
+
+export {
+  default as MemorySyncAdapter
+} from "./MemorySyncAdapter";
+
+export type {
+  MemorySyncRecord
+} from "./MemorySyncAdapter";
+
+export {
+  default as MemoryStorageAdapter
+} from "./MemoryStorageAdapter";
+
+export type {
+  MemoryStorageStatistics
+} from "./MemoryStorageAdapter";
