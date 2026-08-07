33f056da4ebf4080d9a2bb244316a77809a88861
niteshrana657-star <niteshrana657@gmail.com>
2026-08-07 17:33:04 +0530

    Create release/index.ts for public exports
    Add central export point for release management features.

---
+++ release/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Universal AI Operating Companion
+ * Release Module Public Exports
+ *
+ * Central export point for release management features.
+ */
+
+export { ReleasePackageManager } from "./ReleasePackageManager";
+export { VersionManager } from "./VersionManager";
+export { ChangelogManager } from "./ChangelogManager";
+export { PackageValidator } from "./PackageValidator";
+export { ReleaseExporter } from "./ReleaseExporter";
