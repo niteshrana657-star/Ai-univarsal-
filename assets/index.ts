ef5af08d688f98c47f4650c418c17c997df1117c
niteshrana657-star <niteshrana657@gmail.com>
2026-08-07 17:08:17 +0530

    Create index.ts for asset management exports
    This file serves as the main export module for various asset managers and types, including AssetManager, ImageManager, AudioManager, VideoManager, and ResourceLoader.

---
+++ assets/index.ts
@@ -0,0 +1,49 @@
+/**
+ * -------------------------------------------------------------
+ * Universal AI Operating Companion
+ * Assets Module
+ * Exports
+ * File: index.ts
+ * -------------------------------------------------------------
+ */
+
+export {
+  default as AssetManager,
+  AssetType
+} from "./AssetManager";
+
+export type {
+  Asset
+} from "./AssetManager";
+
+export {
+  default as ImageManager
+} from "./ImageManager";
+
+export type {
+  ImageAsset
+} from "./ImageManager";
+
+export {
+  default as AudioManager
+} from "./AudioManager";
+
+export type {
+  AudioAsset
+} from "./AudioManager";
+
+export {
+  default as VideoManager
+} from "./VideoManager";
+
+export type {
+  VideoAsset
+} from "./VideoManager";
+
+export {
+  default as ResourceLoader
+} from "./ResourceLoader";
+
+export type {
+  ResourceLoadResult
+} from "./ResourceLoader";
