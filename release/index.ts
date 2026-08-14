ca78ef23cd35dee9726f208e5470a6c1c5ef9952
niteshrana657-star <niteshrana657@gmail.com>
2026-08-14 16:52:57 +0530

    Refactor release module public exports
    Refactored public exports for release management modules, adding type exports for better type safety.

--- release/index.ts
+++ release/index.ts
@@ -1,12 +1,76 @@
 /**
+ * -------------------------------------------------------------
  * Universal AI Operating Companion
- * Release Module Public Exports
+ * Release Module
+ * File: index.ts
+ * -------------------------------------------------------------
  *
- * Central export point for release management features.
+ * Central public export point for release management.
+ * -------------------------------------------------------------
  */
 
-export { ReleasePackageManager } from "./ReleasePackageManager";
-export { VersionManager } from "./VersionManager";
-export { ChangelogManager } from "./ChangelogManager";
-export { PackageValidator } from "./PackageValidator";
-export { ReleaseExporter } from "./ReleaseExporter";
+// -------------------------------------------------------------
+// Release Package Manager
+// -------------------------------------------------------------
+
+export {
+  ReleasePackageManager,
+  PackageStatus
+} from "./ReleasePackageManager";
+
+export type {
+  ReleasePackage
+} from "./ReleasePackageManager";
+
+
+// -------------------------------------------------------------
+// Version Manager
+// -------------------------------------------------------------
+
+export {
+  VersionManager
+} from "./VersionManager";
+
+export type {
+  Version
+} from "./VersionManager";
+
+
+// -------------------------------------------------------------
+// Changelog Manager
+// -------------------------------------------------------------
+
+export {
+  ChangelogManager
+} from "./ChangelogManager";
+
+export type {
+  ChangelogEntry
+} from "./ChangelogManager";
+
+
+// -------------------------------------------------------------
+// Package Validator
+// -------------------------------------------------------------
+
+export {
+  PackageValidator
+} from "./PackageValidator";
+
+export type {
+  ValidationIssue,
+  ValidationResult
+} from "./PackageValidator";
+
+
+// -------------------------------------------------------------
+// Release Exporter
+// -------------------------------------------------------------
+
+export {
+  ReleaseExporter
+} from "./ReleaseExporter";
+
+export type {
+  ExportResult
+} from "./ReleaseExporter";
